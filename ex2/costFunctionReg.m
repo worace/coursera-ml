ca2ff04eabbbeee1d2ca2fffc3e09e9e090935ce
Horace Williams <horace.d.williams@gmail.com>
2017-02-27 08:31:14 -0800

    finish ex2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -2,12 +2,9 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -19,8 +16,14 @@ grad = zeros(size(theta));
 
 
 
+m = length(y);
 
-
+h = sigmoid(X*theta);
+reg_cost =  sum(theta(2:end).^2)/m/2 * lambda;
+J = (-y' * log(h) - (1-y')*log(1-h))/m + reg_cost;
+% don't regularize first theta term (constant term)
+reg = [0; (lambda/m) * theta(2:end)];
+grad = (X' * (h - y))/m + reg;
 
 % =============================================================
 
--- ex2/ex2_reg.m
+++ ex2/ex2_reg.m
@@ -2,7 +2,7 @@
 %
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the second part
 %  of the exercise which covers regularization with logistic regression.
 %
@@ -27,24 +27,24 @@ clear ; close all; clc
 data = load('ex2data2.txt');
 X = data(:, [1, 2]); y = data(:, 3);
 
-plotData(X, y);
+## plotData(X, y);
 
-% Put some labels 
-hold on;
+## % Put some labels
+## hold on;
 
-% Labels and Legend
-xlabel('Microchip Test 1')
-ylabel('Microchip Test 2')
+## % Labels and Legend
+## xlabel('Microchip Test 1')
+## ylabel('Microchip Test 2')
 
-% Specified in plot order
-legend('y = 1', 'y = 0')
-hold off;
+## % Specified in plot order
+## legend('y = 1', 'y = 0')
+## hold off;
 
 
 %% =========== Part 1: Regularized Logistic Regression ============
 %  In this part, you are given a dataset with data points that are not
-%  linearly separable. However, you would still like to use logistic 
-%  regression to classify the data points. 
+%  linearly separable. However, you would still like to use logistic
+%  regression to classify the data points.
 %
 %  To do so, you introduce more features to use -- in particular, you add
 %  polynomial features to our data matrix (similar to polynomial
@@ -55,6 +55,7 @@ hold off;
 
 % Note that mapFeature also adds a column of ones for us, so the intercept
 % term is handled
+## X = data(:, [1, 2]); y = data(:, 3);
 X = mapFeature(X(:,1), X(:,2));
 
 % Initialize fitting parameters
@@ -74,9 +75,6 @@ fprintf(' %f \n', grad(1:5));
 fprintf('Expected gradients (approx) - first five values only:\n');
 fprintf(' 0.0085\n 0.0188\n 0.0001\n 0.0503\n 0.0115\n');
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
 % Compute and display cost and gradient with non-zero theta
 test_theta = ones(size(X,2),1);
 [cost, grad] = costFunctionReg(test_theta, X, y, lambda);
@@ -88,12 +86,10 @@ fprintf(' %f \n', grad(1:5));
 fprintf('Expected gradients (approx) - first five values only:\n');
 fprintf(' 0.3460\n 0.0851\n 0.1185\n 0.1506\n 0.0159\n');
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
 
 %% ============= Part 2: Regularization and Accuracies =============
 %  Optional Exercise:
-%  In this part, you will get to try different values of lambda and 
+%  In this part, you will get to try different values of lambda and
 %  see how regularization affects the decision coundart
 %
 %  Try the following values of lambda (0, 1, 10, 100).
@@ -133,3 +129,27 @@ p = predict(theta, X);
 fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
 fprintf('Expected accuracy (with lambda = 1): 83.1 (approx)\n');
 
+
+%%%%%%%%%%%%%%%
+% Example test case from week 3 "resources" section in coursera
+theta = [-2; -1; 1; 2];
+X = [ones(5,1) reshape(1:15,5,3)/10];
+y = [1;0;1;0;1] >= 0.5;       % creates a logical array
+lambda = 3;
+[J grad] = costFunctionReg(theta, X, y, lambda)
+
+fprintf('\nTest Case Cost: %f\n', J);
+fprintf('Expected cost: 2.5348\n');
+
+fprintf('Gradient:\n');
+fprintf(' %f \n', grad);
+fprintf('Expected gradients\n');
+fprintf(' 0.14656\n -0.54856\n 0.72472\n 1.398\n');
+% results
+## J =  2.5348
+## grad =
+
+##    0.14656
+##   -0.54856
+##    0.72472
+##    1.39800
