27f1ab57efc31e8035c61355643fff493ae9337d
Horace Williams <horace.d.williams@gmail.com>
2017-03-05 17:33:39 -0800

    test cases for ex 3

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -42,6 +42,7 @@ sel = X(rand_indices(1:100), :);
 displayData(sel);
 
 
+
 %% ============ Part 2a: Vectorize Logistic Regression ============
 %  In this part of the exercise, you will reuse your logistic regression
 %  code from the last exercise. You task here is to make sure that your
@@ -81,3 +82,70 @@ pred = predictOneVsAll(all_theta, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
+
+%% Test Cases (https://www.coursera.org/learn/machine-learning/discussions/all/threads/5g8LaZTCEeW0dw6k4EUmPw) %%
+%%%%%%%%%%%%%%%%%%%
+% 1. lrCostFunction
+%%%%%%%%%%%%%%%%%%%
+theta = [-2; -1; 1; 2];
+X = [ones(5,1) reshape(1:15,5,3)/10];
+y = [1;0;1;0;1] >= 0.5;       % creates a logical array
+lambda = 3;
+[J grad] = lrCostFunction(theta, X, y, lambda)
+
+% Expected:
+## J =  2.5348
+## grad =
+##    0.14656
+##   -0.54856
+##    0.72472
+##    1.39800
+
+
+%%%%%%%%%%%%%%%%%%%
+% 2. oneVsAll
+%%%%%%%%%%%%%%%%%%%
+X = [magic(3) ; sin(1:3); cos(1:3)];
+y = [1; 2; 2; 1; 3];
+num_labels = 3;
+lambda = 0.1;
+[all_theta] = oneVsAll(X, y, num_labels, lambda)
+%expected:
+## all_theta =
+##   -0.559478   0.619220  -0.550361  -0.093502
+##   -5.472920  -0.471565   1.261046   0.634767
+##    0.068368  -0.375582  -1.652262  -1.410138
+
+%%%%%%%%%%%%%%%%%%%
+% 3. predictOneVsAll
+%%%%%%%%%%%%%%%%%%%
+
+all_theta = [1 -6 3; -2 4 -3];
+X = [1 7; 4 5; 7 8; 1 4];
+predictOneVsAll(all_theta, X)
+%output:
+## ans =
+##    1
+##    2
+##    2
+##    1
+
+
+%%%%%%%%%%%%%%%%%%%
+% 4. predict
+%%%%%%%%%%%%%%%%%%%
+
+Theta1 = reshape(sin(0 : 0.5 : 5.9), 4, 3);
+Theta2 = reshape(sin(0 : 0.3 : 5.9), 4, 5);
+X = reshape(sin(1:16), 8, 2);
+p = predict(Theta1, Theta2, X)
+% you should see this result
+## p =
+##   4
+##   1
+##   1
+##   4
+##   4
+##   4
+##   4
+##   2
